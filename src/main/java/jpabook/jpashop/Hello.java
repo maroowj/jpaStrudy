67c83b07fd6a855e7166e765be955ec5563f9328
maroowj <left_right@naver.com>
2023-07-05 18:50:38 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+public class Hello {
+    private String data;
+}
